763732f25b8377327c131347594549e3cf9d94ad
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-03-26 00:03:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (69.34%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     char repeatedCharacter(string s) {
-        // int freq[26]={0};
-        unordered_map<char,int> freq;
+        int freq[26]={0};
+        // unordered_map<char,int> freq;
         for(char ch:s){
-            freq[ch]++;
-            if(freq[ch]==2){
+            freq[ch-'a']++;
+            if(freq[ch-'a']==2){
                 return ch;
             }
         }
