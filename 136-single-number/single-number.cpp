1d6522a3af63c45a791805f6d2abf9dc2023081c
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-04-01 00:00:34 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (58.73%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_set<int> unique;
-        int sum=0;
-        int usum=0;
-        for(int n:nums){
-            if(unique.find(n)==unique.end()){
-                usum+=n;
-                unique.insert(n);
-            }
-            sum+=n;
+        int xor1=0;
+        for(int i:nums){
+            xor1^=i;
         }
-        return 2*usum - sum;
+        return xor1;
     }
 };
