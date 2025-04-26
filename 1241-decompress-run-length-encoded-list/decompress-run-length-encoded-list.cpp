8790763df846c2fdddc03fb5e8d70139eb828d30
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-04-26 23:21:01 +0530

    Time: 3 ms (12.18%) | Memory: 13.9 MB (11.68%) - LeetSync

--- 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.cpp
+++ 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.cpp
@@ -2,15 +2,9 @@ class Solution {
 public:
     vector<int> decompressRLElist(vector<int>& nums) {
         vector<int> res;
-        int x;
-        for(int i=0;i<nums.size();i++){
-            if(i%2==0){
-                x=nums[i];
-            }else{
-                for(int j=0;j<x;j++){
-                    res.push_back(nums[i]);
-                }
-            }
+        for(int i=0;i<nums.size();i+=2){
+            int count=nums[i],num=nums[i+1];
+            res.insert(res.end(),count,num);
         }
         return res;
     }
