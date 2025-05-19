8a047320c883f02912c388f697a01fd55eea87ce
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-05-19 12:03:01 +0530

    Time: 3 ms (71.86%) | Memory: 8 MB (37.82%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
+    bool isprime(int setBits){
+        if(setBits<2) return false;
+        for(int j=2;j<=sqrt(setBits);j++){
+            if(setBits%j==0){
+                return false;
+            }
+        }
+        return true;
+    }
     int countPrimeSetBits(int left, int right) {
         int pcount=0;
         for(int i=left;i<=right;i++){
             int count=0;
             int setBits=__builtin_popcount(i);
-            if(setBits<2) continue;
-            for(int j=2;j<=sqrt(setBits);j++){
-                if(setBits%j==0){
-                    count++;
-                }
-            }
-            if(count==0){
+            
+            if(isprime(setBits)){
                 pcount++;
             }
         }
