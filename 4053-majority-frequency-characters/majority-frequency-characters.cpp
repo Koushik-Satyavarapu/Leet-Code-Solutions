6c4d95c835b8c251529d3f4753dca0ddc23c8e6e
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-10-06 00:43:17 +0530

    Time: 8 ms (14.70%) | Memory: 10.1 MB (55.65%) - LeetSync

--- 4053-majority-frequency-characters/majority-frequency-characters.cpp
+++ 4053-majority-frequency-characters/majority-frequency-characters.cpp
@@ -5,29 +5,25 @@ public:
         for(char ch:s){
             freq[ch]++;
         }
-        unordered_map<int,vector<char>> mp;
-        for(auto it:freq){
-            mp[it.second].push_back(it.first);
+        unordered_map<int,string> mp;
+        for(auto [x,y]:freq){
+            mp[y]+=x;
         }
         int grpsiz=0;
         int dist=0;
-        for(auto it:mp){
-            if(it.second.size()>grpsiz){
-                grpsiz=it.second.size();
-                dist=it.first;
-            }else if(it.second.size()==grpsiz && dist<it.first){
-                grpsiz=it.second.size();
-                dist=it.first;
-            }
-        }
         string str;
-        for(auto it:mp){
-            if(it.second.size()==grpsiz && it.first==dist){
-                for(char ch:it.second){
-                    str+=ch;
-                }
+        for(auto [x,y]:mp){
+            if(y.size()>grpsiz){
+                grpsiz=y.size();
+                dist=x;
+                str=y;
+            }else if(y.size()==grpsiz && dist<x){
+                grpsiz=y.size();
+                dist=x;
+                str=y;
             }
         }
+        
         return str;
     }
 };
