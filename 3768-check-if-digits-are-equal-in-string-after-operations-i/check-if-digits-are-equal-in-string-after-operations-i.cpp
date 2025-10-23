cdf46e0d63b79226bab240051c9dcbe46eaf2bca
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-10-23 23:51:27 +0530

    Time: 2 ms (89.06%) | Memory: 8.2 MB (98.48%) - LeetSync

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        if(s.size()<2){
+            return false;
+        }
+        while(s.size()>2){
+            for(int i=0;i<s.size()-1;i++){
+                s[i]=(s[i]-'0' + s[i+1]-'0')%10;
+            }
+            s.pop_back();
+        }
+        if(s[0]==s[1]){
+            return true;
+        }
+        return false;
+    }
+};
