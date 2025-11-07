f9afe39868e5164fde1ac61466532aedd40ae98d
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-11-07 20:13:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.49%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n <= 2) return n;
-        
-        int prev1 = 1, prev2 = 2, curr;
-        for (int i = 3; i <= n; i++) {
-            curr = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = curr;
+    int func(int n,vector<int> &dp){
+        if(n<0){
+            return 0;
         }
-        return curr;
+        if(n==0){
+            return 1;
+        }
+        if(dp[n]!=-1) return dp[n];
+        int onestep=func(n-1,dp);
+        int twostep=func(n-2,dp);
+        return dp[n]=onestep+twostep;
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return func(n,dp);
     }
 };
