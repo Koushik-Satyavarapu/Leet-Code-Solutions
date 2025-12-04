d98b3e56a28fe1c0d2617d834ef6ba4b81e69297
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-12-04 13:23:24 +0530

    Time: 2 ms (82.47%) | Memory: 15.7 MB (50.13%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    void printpar(set<string> &res,int n,int l,int r,string st){
+    void printpar(vector<string> &res,int n,int l,int r,string st){
         if(l==n && r==n){
-            res.insert(st);
+            res.push_back(st);
             return;
         }else if(l>n ||r>n){
             return;
         }
-        if(l==0){
+        if(l<n){
             printpar(res,n,l+1,r,st+'(');
         }
-        if(l>r && l<n){
-            printpar(res,n,l+1,r,st+'(');
-            printpar(res,n,l,r+1,st+')');
-        }else if(l==r && l<n){
-            printpar(res,n,l+1,r,st+'(');
-        }else if(r<n){
+        if(r<l){
             printpar(res,n,l,r+1,st+')');
         }
     }
     vector<string> generateParenthesis(int n) {
-        set<string> res;
+        vector<string> res;
         string st="";
         printpar(res,n,0,0,st);
-        vector<string> resi(res.begin(),res.end());
-        return resi;
+        return res;
     }
 };
