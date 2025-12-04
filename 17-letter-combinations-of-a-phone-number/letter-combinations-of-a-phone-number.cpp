b69510ade75fcf9dd71cfb49fbb14d709913dd7d
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-12-04 20:34:02 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (7.05%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    void combPN(unordered_map<char,string> mp,string st,int ind,string digits,vector<string> &res){
+    void seq(vector<string> &res,vector<pair<int,string>> digi,string digits,int ind,string st){
         if(ind==digits.size()){
             res.push_back(st);
             return;
         }
-        string temp=mp[digits[ind]];
-        for(int i=0;i<temp.size();i++){
-            st+=temp[i];
-            combPN(mp,st,ind+1,digits,res);
-            st.pop_back();
+        for(int i=0;i<digi[digits[ind]-'2'].second.size();i++){
+            seq(res,digi,digits,ind+1,st+digi[digits[ind]-'2'].second[i]);
         }
     }
     vector<string> letterCombinations(string digits) {
-        unordered_map<char,string> mp{{'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}};
+        if(digits.size()==0)return {};
+        vector<pair<int,string>> digi={{2,"abc"},{3,"def"},{4,"ghi"},{5,"jkl"},{6,"mno"},{7,"pqrs"},{8,"tuv"},{9,"wxyz"}};
         vector<string> res;
-        if(digits.size()==0) return res;
-        string st;
-        combPN(mp,st,0,digits,res);
+        string st="";
+        seq(res,digi,digits,0,st);
         return res;
     }
 };
