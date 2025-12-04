f0f712f6bf3d6b8f9fa4511e54ca068915d26bfc
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2025-12-04 20:58:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (16.43%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    void seq(vector<string> &res,vector<pair<int,string>> digi,string digits,int ind,string st){
+    void seq(vector<string> &res,vector<string> digi,string digits,int ind,string st){
         if(ind==digits.size()){
             res.push_back(st);
             return;
         }
-        for(int i=0;i<digi[digits[ind]-'2'].second.size();i++){
-            seq(res,digi,digits,ind+1,st+digi[digits[ind]-'2'].second[i]);
+        for(int i=0;i<digi[digits[ind]-'2'].size();i++){
+            seq(res,digi,digits,ind+1,st+digi[digits[ind]-'2'][i]);
         }
     }
     vector<string> letterCombinations(string digits) {
         if(digits.size()==0)return {};
-        vector<pair<int,string>> digi={{2,"abc"},{3,"def"},{4,"ghi"},{5,"jkl"},{6,"mno"},{7,"pqrs"},{8,"tuv"},{9,"wxyz"}};
+        vector<string> digi={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         vector<string> res;
         string st="";
         seq(res,digi,digits,0,st);
