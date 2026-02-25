b5d60e1eb7e50c53542dc65c749295a5e4ed53dd
Koushik-Satyavarapu <koushik.satyavarapu@gmail.com>
2026-02-25 22:54:33 +0530

    Time: 8 ms (24.50%) | Memory: 20.4 MB (8.05%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.cpp
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     vector<string> reorderLogFiles(vector<string>& logs) {
-        typedef pair<pair<string,string>,int> P;
-        priority_queue<P,vector<P>,greater<P>> pq;
-        vector<string> res;
-        vector<int> indDig;
+        vector<string> num;
+        vector<string> lett;
         for(int i=0;i<logs.size();i++){
-            auto space=logs[i].find(' ');
-            string ident=logs[i].substr(0,space);
-            string rem=logs[i].substr(space+1);
-            if(isdigit(rem[0])){
-                indDig.push_back(i);
+            int space=logs[i].find(' ');
+            if(isdigit(logs[i][space+1])){
+                num.push_back(logs[i]);
             }else{
-                pq.push({{rem,ident},i});
+                lett.push_back(logs[i]);
             }
         }
-        while(!pq.empty()){
-            res.push_back(logs[pq.top().second]);
-            pq.pop();
-        }
-        for(int i=0;i<indDig.size();i++){
-            res.push_back(logs[indDig[i]]);
-        }
+        sort(lett.begin(),lett.end(),[](string a,string b){
+            auto space1=a.find(' ');
+            auto space2=b.find(' ');
+            string ident1=a.substr(0,space1);
+            string ident2=b.substr(0,space2);
+            string rem1=a.substr(space1+1);
+            string rem2=b.substr(space2+1);
+            if(rem1==rem2){
+                return ident1<ident2;
+            }else{
+                return rem1<rem2;
+            }
+
+        });
+        vector<string> res;
+        res.insert(res.end(),lett.begin(),lett.end());
+        res.insert(res.end(),num.begin(),num.end());
         return res;
     }
 };
